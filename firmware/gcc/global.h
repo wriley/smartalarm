7ddb4e85e602865dc15d23612793659f47606f0c
riley <riley@34e2be26-f520-4db7-ad9b-b5ecd4381fcd>
2008-05-20 04:29:10 +0000

    removed arduino version first firmware that runs on pcb

--- firmware/gcc/global.h
+++ firmware/gcc/global.h
@@ -10,19 +10,35 @@
 
 // CPU clock speed
 //#define F_CPU        16000000               		// 16MHz processor
+#define F_CPU        12000000               		// 12MHz processor
 //#define F_CPU        14745000               		// 14.745MHz processor
 //#define F_CPU        8000000               		// 8MHz processor
 //#define F_CPU        7372800               		// 7.37MHz processor
-#define F_CPU        4000000               		// 4MHz processor
+//#define F_CPU        4000000               		// 4MHz processor
 //#define F_CPU        3686400               		// 3.69MHz processor
 #define CYCLES_PER_US ((F_CPU+500000)/1000000) 	// cpu cycles per microsecond
 
 // Status LED colors
-#define RED 255
-#define ORANGE 127
-#define GREEN 0
+#define RED 0
+#define YELLOW 1
+#define GREEN 2
+
+// Alarm modes
+#define OFF 0
+#define ON 1
+#define REPEAT 2
+#define PULSE 3
+#define SONG 4
+
+// for repeat mode
+#define START 2
 
 // ms to pause between tests in test cycle
 #define TESTPAUSE 3000
+
+// timer defines
+#define TIMER_PRESCALE		1024
+#define TIMER_INTERVAL		(F_CPU/TIMER_PRESCALE/100)	// 100ms interval
+
 
 #endif
--- firmware/gcc/main.c
+++ firmware/gcc/main.c
@@ -1,26 +1,38 @@
+#include "global.h"		// include our global settings
+
 #include <avr/io.h>			// include I/O definitions (port names, pin names, etc)
 #include <avr/interrupt.h>	// include interrupt support
+#include <util/delay.h>
 
-#include "global.h"		// include our global settings
 #include "uart.h"		// include uart function library
 #include "rprintf.h"	// include printf function library
 #include "timer.h"		// include timer function library (timing, PWM, etc)
-#include "pulse.h"		// include pulse output support
 #include "cmdline.h"	// include cmdline function library
 
 // global variables
 u08 Run;
+volatile u16 repeatOn = 2000;
+volatile u16 repeatOff = 1000;
+volatile u08 repeatCycle = ON;
+volatile u16 repeatCounter = 0;
+volatile u16 pulseOn = 0;
+volatile u08 pulseStart = 0;
+volatile u16 pulseCounter = 0;
+volatile u08 mode = OFF;
+volatile u08 lastmode = OFF;
+volatile u16 UptimeMs = 0;
 
 // functions
 void goCmdline(void);
 void statusLED(u08);
+void systickHandler(void);
 
 void exitFunction(void);
 void helpFunction(void);
 void testFunction(void);
-void freqFunction(void);
-void alarmonFunction(void);
-void alarmoffFunction(void);
+void alarmFunction(void);
+void repeatFunction(void);
+void pulseFunction(void);
 
 void alarmOn(void);
 void alarmOff(void);
@@ -38,24 +50,19 @@ int main(void)
 	// initialize rprintf system
 	rprintfInit(uartSendByte);
 
-	// set pulse pin to output	
-	sbi(DDRD, 4);
 
-	// set statusLED pins to output
+	// initialize systick timer
+	timer2SetPrescaler(TIMERRTC_CLK_DIV1024);
+	timerAttach(TIMER2OVERFLOW_INT, systickHandler);
+
+	// set status LED pins to output
 	sbi(DDRB, 1);
 	sbi(DDRB, 2);
 
-	sbi(DDRB, 4);
+	sbi(DDRB, 0);
+	cbi(PORTB, 0);
 
-	// initialize pulse library
-	pulseInit();
-	pulseT1ASetFreq(1500);
-
-	timer1PWMInit(8);
-	timer1PWMBOn();
-	timer1PWMBSet(0);
-
-	statusLED(ORANGE);
+	statusLED(YELLOW);
 
 	// start command line
 	goCmdline();
@@ -67,7 +74,7 @@ void goCmdline(void)
 {
 	u08 c;
 
-	rprintf("smartAlarm ready\r\n");
+	rprintfProgStrM("\r\nsmartAlarm ready\r\n");
 	
 	// initialize cmdline system
 	cmdlineInit();
@@ -78,10 +85,10 @@ void goCmdline(void)
 	// add commands to the command database
 	cmdlineAddCommand("help",		helpFunction);
 	cmdlineAddCommand("exit",		exitFunction);
-	cmdlineAddCommand("alarmon",	alarmonFunction);
-	cmdlineAddCommand("alarmoff",	alarmoffFunction);
-	cmdlineAddCommand("freq",		freqFunction);
+	cmdlineAddCommand("alarm",		alarmFunction);
 	cmdlineAddCommand("test",		testFunction);
+	cmdlineAddCommand("repeat",		repeatFunction);
+	cmdlineAddCommand("pulse",		pulseFunction);
 
 	// send a CR to cmdline input to stimulate a prompt
 	cmdlineInputFunc('\r');
@@ -102,24 +109,23 @@ void goCmdline(void)
 		cmdlineMainLoop();
 	}
 
-	rprintfCRLF();
-	rprintf("smartAlarm exiting!!!\r\n");
+	rprintfProgStrM("\r\nsmartAlarm exiting!!!\r\n");
 	statusLED(RED);
 }
 
 void statusLED(u08 color){
 	switch(color){
 		case RED:
-			timer1PWMBSet(0);
-			sbi(PORTB, 4);
+			cbi(PORTB, 1);
+			sbi(PORTB, 2);
 			break;
 		case GREEN:
-			timer1PWMBSet(255);
-			cbi(PORTB, 4);
+			sbi(PORTB, 1);
+			cbi(PORTB, 2);
 			break;
-		case ORANGE:
-			timer1PWMBSet(127);
-			sbi(PORTB, 4);
+		case YELLOW:
+			cbi(PORTB, 1);
+			cbi(PORTB, 2);
 			break;
 		default:
 			break;
@@ -137,19 +143,18 @@ void helpFunction(void)
 {
 	rprintfCRLF();
 
-	rprintf("Available commands are:\r\n");
-	rprintf("help      - displays available commands\r\n");
-	rprintf("exit      - shutdown smartAlarm (requires reset to resume)\r\n");
-	rprintf("alarmon   - activate Alarm\r\n");
-	rprintf("alarmoff  - de-activate Alarm\r\n");
-	rprintf("freq      - set frequency in Hertz for Alarm\r\n");
-	rprintf("test      - run test cycle for LEDs and Alarm\r\n");
+	rprintfProgStrM("Available commands are:\r\n");
+	rprintfProgStrM("help      - displays available commands\r\n");
+	rprintfProgStrM("exit      - shutdown smartAlarm (requires reset to resume)\r\n");
+	rprintfProgStrM("alarm     - set alarm on (1) or off (0)\r\n");
+	rprintfProgStrM("test      - run test cycle for LEDs and Alarm\r\n");
+	rprintfProgStrM("repeat    - sound alarm for <on>ms and <off>ms (optional duration <ms>)\r\n");
+	rprintfProgStrM("pulse     - sound alarm for <on>ms\r\n");
 
 	rprintfCRLF();
 }
 
 void testFunction(void){
-	rprintf("Beginning test sequence\r\n");
 	rprintfCRLF();
 
 	rprintf("Setting statusLED to GREEN\r\n");
@@ -160,38 +165,129 @@ void testFunction(void){
 	statusLED(RED);
 	timerPause(TESTPAUSE);
 
-	rprintf("Setting statusLED to GREEN\r\n");
+	rprintf("Setting statusLED to YELLOW\r\n");
+	statusLED(YELLOW);
+	timerPause(TESTPAUSE);
+
 	statusLED(GREEN);
 	rprintf("Turning on alarm\r\n");
 	alarmOn();
 	timerPause(TESTPAUSE);
 	rprintf("Turning off alarm\r\n");
 	alarmOff();
+	timerPause(1000);
 	rprintf("Test cycle complete\r\n");
 
 	rprintfCRLF();
 }
 
-void alarmonFunction(void){
-	alarmOn();
-	rprintf("Alarm ON\r\n");
-}
-
-void alarmoffFunction(void){
-	alarmOff();
-	rprintf("Alarm OFF\r\n");
-}
-
-void freqFunction(void){
-	u16 f = cmdlineGetArgInt(1);
-	pulseT1ASetFreq(f);
-	rprintf("Frequency set to %dHz\r\n", f);
+void alarmFunction(void){
+	u08 s = cmdlineGetArgInt(1);
+	switch(s){
+		case 0:
+			alarmOff();
+			rprintf("OK - mode = %d\r\n", mode);
+			break;
+		case 1:
+			alarmOn();
+			rprintf("OK - mode = %d\r\n", mode);
+			break;
+	}
 }
 
 void alarmOn(void){
-	pulseT1ARun(0);
+	mode = ON;
 }
 
 void alarmOff(void){
-	pulseT1AStop();
+	mode = OFF;
+}
+
+void repeatFunction(void){
+	repeatOn = cmdlineGetArgInt(1);
+	repeatOff = cmdlineGetArgInt(2);
+	repeatDuration = cmdlineGetArgInt(3);
+	repeatCycle = START;
+	mode = REPEAT;
+	rprintf("OK - mode = %d\r\n", mode);
+}
+
+void pulseFunction(void){
+	pulseOn = cmdlineGetArgInt(1);
+	pulseStart = 1;
+	mode = PULSE;
+	rprintf("OK - mode = %d\r\n", mode);
+}
+
+void systickHandler(void){
+	// set timer for 10ms interval
+	TCNT2 = (unsigned char)-TIMER_INTERVAL;
+	// count up on uptime
+	UptimeMs += 10;
+
+	switch(mode){
+		case OFF:
+			if(lastmode != mode){
+				cbi(PORTB, 0);
+				sbi(PORTC, 1);
+				sbi(PORTC, 2);
+			}
+			break;
+		case ON:
+			if(lastmode != mode){
+				sbi(PORTB, 0);
+				cbi(PORTC, 1);
+			}
+			break;
+		case REPEAT:
+			if(repeatCycle == START){
+				repeatCounter = 0;
+				repeatCycle = ON;
+				sbi(PORTB, 0);
+				sbi(PORTC, 1);
+				cbi(PORTC, 2);
+			} else {
+				repeatCounter += 10;
+			}
+			
+			switch(repeatCycle){
+				case OFF:
+					if(repeatCounter > repeatOff){
+						sbi(PORTB, 0);
+						repeatCounter = 0;
+						repeatCycle = ON;
+					}
+					break;
+				case ON:
+					if(repeatCounter > repeatOn){
+						cbi(PORTB, 0);
+						repeatCounter = 0;
+						repeatCycle = OFF;
+					}
+					break;
+			}
+			break;
+		case PULSE:
+			if(pulseStart == 1){
+				sbi(PORTB, 0);
+				pulseStart = 0;
+				pulseCounter = 0;
+			} else {
+				if(pulseCounter > pulseOn){
+					cbi(PORTB, 0);
+				} else {
+					pulseCounter += 10;
+				}
+			}
+
+			break;
+		case SONG:
+			break;
+		default:
+			rprintf("Uknown mode!: %d\r\n", mode);
+			break;
+	}
+	if(lastmode != mode){
+		lastmode = mode;
+	}					
 }
